35597cd25bd74b983f5b44f671797d6c602758ae
JefferNg <jefferng11@gmail.com>
2023-04-22 12:04:45 -0700

    player prefab and script

---
+++ Playing with Fire SGJ23/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>() as Rigidbody2D;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+}
