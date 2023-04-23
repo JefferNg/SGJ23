32172c07274fe4a015f32ac4f4e6661212e5a66f
Kanaz312 <63126907+Kanaz312@users.noreply.github.com>
2023-04-22 18:11:01 -0700

    Added Player Death

---
+++ Playing with Fire SGJ23/Assets/Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerManager), typeof(Rigidbody2D))]
+public class PlayerDeath : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask _deathLayerMask;
+
+    private PlayerManager _pm = null;
+
+    private void Start()
+    {
+        _pm = GetComponent<PlayerManager>();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        bool isOnDeathLayer = (_deathLayerMask & (1 << collision.gameObject.layer)) != 0;
+        if (isOnDeathLayer)
+        {
+            _pm.Die();
+        }
+    }
+}
